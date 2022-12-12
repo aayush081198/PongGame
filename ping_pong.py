3c1db1e3db03a6a43aea6c75288a7f318c497a63
aayush081198 <120128508+aayush081198@users.noreply.github.com>
2022-12-12 16:12:16 -0500

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -5,7 +5,7 @@ import gym
 
 
 # hyperparameters
-H = 600 # number of hidden layer neurons
+H = 200 # number of hidden layer neurons
 batch_size = 10 # every how many episodes to do a param update?
 learning_rate = 1e-2
 gamma = 0.99 # discount factor for reward
